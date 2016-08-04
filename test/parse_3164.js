72e525ce4c25aa5ed5a42ca1cd7d46ad45e10a69
Mike Hu <mhu@neurox.net>
2016-08-04 16:23:17 -0700

    parse out appName and pid from RFC3164 format cleaned up some stuff based on jshint

--- test/parse_3164.js
+++ test/parse_3164.js
@@ -9,9 +9,9 @@ var gen = new syslogGenerator({type: 'bsd'});
 var doubleSpaced = "<13>Feb  5 17:32:18 10.0.0.99 Use the BFG!";
 syslogParser.parse(doubleSpaced, function(parsedMessage){
     var msg = gen.produce(parsedMessage);
-    assert.equal(doubleSpaced, msg);    
+    assert.equal(doubleSpaced, msg);
 
-    var expectedData = { 
+    var expectedData = {
         originalMessage: doubleSpaced,
         prival: 13,
         facilityID: 1,
@@ -40,7 +40,32 @@ syslogParser.parse(withCommand, function(parsedMessage){
         severity: 'crit',
         type: 'RFC3164',
         host: 'mymachine',
-        message: "su: 'su root' failed for lonvick on /dev/pts/8" };
+        appName: 'su',
+        message: "'su root' failed for lonvick on /dev/pts/8" };
+
+    var parsedDate = parsedMessage.time;
+    delete parsedMessage.time;
+
+    assert.equal(parsedDate.getUTCMonth(), 9);
+    assert.equal(parsedDate.getUTCHours(), 20);
+    assert.deepEqual(parsedMessage, expectedData);
+
+});
+
+var withPidCommand = "<34>Oct 11 22:14:15 mymachine su[321]: 'su root' failed for lonvick on /dev/pts/8";
+syslogParser.parse(withPidCommand, function(parsedMessage){
+    var expectedData = {
+        originalMessage: withPidCommand,
+        prival: 34,
+        facilityID: 4,
+        severityID: 2,
+        facility: 'auth',
+        severity: 'crit',
+        type: 'RFC3164',
+        host: 'mymachine',
+        appName: 'su',
+        pid: 321,
+        message: "'su root' failed for lonvick on /dev/pts/8" };
 
     var parsedDate = parsedMessage.time;
     delete parsedMessage.time;
@@ -53,7 +78,7 @@ syslogParser.parse(withCommand, function(parsedMessage){
 
 var withDifficultTime = "<191>94103: 51w2d: DHCPD: assigned IP address 10.10.1.94 to client 0100.01c4.21d3.b3";
 syslogParser.parse(withDifficultTime, function(parsedMessage){
-    var expectedData = { 
+    var expectedData = {
         originalMessage: withDifficultTime,
         prival: 191,
         facilityID: 23,
@@ -69,7 +94,7 @@ syslogParser.parse(withDifficultTime, function(parsedMessage){
 
 var withYear = "<32>Mar 05 2011 22:21:02: %ASA-6-302013: Built inbound TCP connection 401 for outside:123.123.123.123/4413 (123.123.123.123/4413) to net:BOX/25 (BOX/25)";
 syslogParser.parse(withYear, function(parsedMessage){
-    var expectedData = { 
+    var expectedData = {
         originalMessage: withYear,
         prival: 32,
         facilityID: 4,
@@ -79,14 +104,15 @@ syslogParser.parse(withYear, function(parsedMessage){
         type: 'RFC3164',
         time: undefined,
         host: '22:21:02:',
-        message: '%ASA-6-302013: Built inbound TCP connection 401 for outside:123.123.123.123/4413 (123.123.123.123/4413) to net:BOX/25 (BOX/25)' };
+        appName: '%ASA-6-302013',
+        message: 'Built inbound TCP connection 401 for outside:123.123.123.123/4413 (123.123.123.123/4413) to net:BOX/25 (BOX/25)' };
 
    assert.deepEqual(parsedMessage, expectedData);
 });
 
 var withSpaces = "<13>Mar 15 11:22:40 myhost.com     0    11,03/15/12,11:22:38,§ó·s,10.10.10.171,,40C6A91373B6,";
 syslogParser.parse(withSpaces, function(parsedMessage){
-    var expectedData = { 
+    var expectedData = {
         originalMessage: withSpaces,
         prival: 13,
         facilityID: 1,
