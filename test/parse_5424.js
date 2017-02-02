af445e18ede6775960891e05266b1e6198b27f4c
Mike Hu <mhu@neurox.net>
2017-02-01 20:40:53 -0800

    when presented with invalid structured data, make best effort to ignore and proceed forward rather than gobbling up the message

--- test/parse_5424.js
+++ test/parse_5424.js
@@ -4,7 +4,7 @@ var syslogParser = require('../lib/glossy/parse.js'),
 assert.ok(syslogParser, 'parser loaded');
 var withPrecisionTime = "<165>1 2003-08-24T05:14:15.000003-07:00 192.0.2.1 myproc 8710 - - %% It's time to make the do-nuts.";
 syslogParser.parse(withPrecisionTime, function(parsedMessage){
-    var expectedData = { 
+    var expectedData = {
         originalMessage: withPrecisionTime,
         prival: 165,
         facilityID: 20,
@@ -17,7 +17,7 @@ syslogParser.parse(withPrecisionTime, function(parsedMessage){
         pid: '8710',
         msgID: null,
         message: "%% It's time to make the do-nuts." };
-    
+
     delete parsedMessage.time;
     assert.deepEqual(parsedMessage, expectedData);
 
@@ -26,7 +26,7 @@ syslogParser.parse(withPrecisionTime, function(parsedMessage){
 // FIXME 3 minute offset from UTC?!
 var with8601 = "<34>1 2003-10-11T22:14:15.003Z mymachine.example.com su - ID47 - BOM'su root' failed for lonvick on /dev/pts/8";
 syslogParser.parse(with8601, function(parsedMessage){
-    var expectedData = { 
+    var expectedData = {
         originalMessage: with8601,
         prival: 34,
         facilityID: 4,
@@ -40,14 +40,14 @@ syslogParser.parse(with8601, function(parsedMessage){
         pid: null,
         msgID: 'ID47',
         message: "BOM'su root' failed for lonvick on /dev/pts/8" };
-    
+
     assert.deepEqual(parsedMessage, expectedData);
 });
 
 // FIXME 3 minute offset from UTC?!
 var withSD = '<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"] BOMAn application event log entry...';
 syslogParser.parse(withSD, function(parsedMessage){
-    var expectedData = { 
+    var expectedData = {
         originalMessage: withSD,
         prival: 165,
         facilityID: 20,
@@ -60,13 +60,13 @@ syslogParser.parse(withSD, function(parsedMessage){
         appName: 'evntslog',
         pid: null,
         msgID: 'ID47',
-        structuredData: { 
-            'exampleSDID@32473': { 
+        structuredData: {
+            'exampleSDID@32473': {
                 iut: '3',
                 eventSource: 'Application',
-                eventID: '1011' 
-            } 
-        }, 
+                eventID: '1011'
+            }
+        },
         message: 'BOMAn application event log entry...' };
 
     assert.deepEqual(parsedMessage, expectedData);
@@ -75,18 +75,18 @@ syslogParser.parse(withSD, function(parsedMessage){
 // FIXME 3 minute offset from UTC?!
 var withDoubleSD =  '<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"]';
 syslogParser.parse(withDoubleSD, function(parsedMessage){
-    var expectedStructuredData = { 
-        'exampleSDID@32473': { 
-            iut: '3', 
+    var expectedStructuredData = {
+        'exampleSDID@32473': {
+            iut: '3',
             eventSource: 'Application',
-            eventID: '1011' 
+            eventID: '1011'
          },
          'examplePriority@32473': {
              'class': 'high'
          }
     };
 
-    var expectedData = { 
+    var expectedData = {
         originalMessage: withDoubleSD,
         prival: 165,
         facilityID: 20,
@@ -104,3 +104,21 @@ syslogParser.parse(withDoubleSD, function(parsedMessage){
     assert.deepEqual(parsedMessage, expectedData);
 });
 
+var withInvalidSD = '<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 abc [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"] BOMAn application event log entry...';
+syslogParser.parse(withInvalidSD, function(parsedMessage){
+    var expectedData = {
+        originalMessage: withInvalidSD,
+        prival: 165,
+        facilityID: 20,
+        severityID: 5,
+        facility: 'local4',
+        severity: 'notice',
+        type: 'RFC5424',
+        time:  new Date('2003-10-11T22:14:15.003Z'),
+        host: 'mymachine.example.com',
+        appName: 'evntslog',
+        pid: null,
+        msgID: 'ID47',
+        message: '[exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"] BOMAn application event log entry...' };
+    assert.deepEqual(parsedMessage, expectedData);
+});
