8c4e23eea27d8f817923e30497cead90ea1aebaa
Kris Reeves <kris@pressbuttonllc.com>
2019-05-25 16:26:42 +0000

    Buffer.from shim

--- test/parse_5848.js
+++ test/parse_5848.js
@@ -1,3 +1,5 @@
+var bufferFrom = require('../lib/glossy/buffer_from');
+
 var syslogParser = require('../lib/glossy/parse.js'),
           assert = require('assert');
 
@@ -6,8 +8,8 @@ assert.ok(syslogParser, 'parser loaded');
 var fullySigned = '<110>1 2009-05-03T14:00:39.519307+02:00 host.example.org syslogd 2138 - [ssign-cert VER="0111" RSID="1" SG="0" SPRI="0" TPBL="587" INDEX="1" FLEN="587" FRAG="2009-05-03T14:00:39.519005+02:00 K BACsLMZ NCV2NUAwe4RAeAnSQuvv2KS51SnHFAaWJNU2XVDYvW1LjmJgg4vKvQPo3HEOD+2hEkt1zcXADe03u5pmHoWy5FGiyCbglYxJkUJJrQqlTSS6vID9yhsmEnh07w3pOsxmb4qYo0uWQrAAenBweVMlBgV3ZA5IMA8xq8l+i8wCgkWJjCjfLar7s+0X3HVrRroyARv8EAIYoxofh9m N8n821BTTuQnz5hp40d6Z3UudKePu2di5Mx3GFelwnV0Qh5mSs0YkuHJg0mcXyUAoeYry5X6482fUxbm+gOHVmYSDtBmZEB8PTEt8Os8aedWgKEt/E4dT+Hmod4omECLteLXxtScTMgDXyC+bSBMjRRCaeWhHrYYdYBACCWMdTc12hRLJTn8LX99kv1I7qwgieyna8GCJv/rEgC ssS9E1qARM+h19KovIUOhl4VzBw3rK7v8Dlw/CJyYDd5kwSvCwjhO21LiReeS90VPYuZFRC1B82Sub152zOqIcAWsgd4myCCiZbWBsuJ8P0gtarFIpleNacCc6OV3i2Rg==" SIGN="AKAQEUiQptgpd0lKcXbuggGXH/dCdQCgdysrTBLUlbeGAQ4vwrnLOqSL7+c="]';
 
 var fullySignedSD = syslogParser.parseStructure(fullySigned);
-assert.deepEqual(fullySignedSD, { 
-    'ssign-cert': { 
+assert.deepEqual(fullySignedSD, {
+    'ssign-cert': {
         VER: '0111',
         RSID: '1',
         SG: '0',
@@ -16,8 +18,8 @@ assert.deepEqual(fullySignedSD, {
         INDEX: '1',
         FLEN: '587',
         FRAG: '2009-05-03T14:00:39.519005+02:00 K BACsLMZ NCV2NUAwe4RAeAnSQuvv2KS51SnHFAaWJNU2XVDYvW1LjmJgg4vKvQPo3HEOD+2hEkt1zcXADe03u5pmHoWy5FGiyCbglYxJkUJJrQqlTSS6vID9yhsmEnh07w3pOsxmb4qYo0uWQrAAenBweVMlBgV3ZA5IMA8xq8l+i8wCgkWJjCjfLar7s+0X3HVrRroyARv8EAIYoxofh9m N8n821BTTuQnz5hp40d6Z3UudKePu2di5Mx3GFelwnV0Qh5mSs0YkuHJg0mcXyUAoeYry5X6482fUxbm+gOHVmYSDtBmZEB8PTEt8Os8aedWgKEt/E4dT+Hmod4omECLteLXxtScTMgDXyC+bSBMjRRCaeWhHrYYdYBACCWMdTc12hRLJTn8LX99kv1I7qwgieyna8GCJv/rEgC ssS9E1qARM+h19KovIUOhl4VzBw3rK7v8Dlw/CJyYDd5kwSvCwjhO21LiReeS90VPYuZFRC1B82Sub152zOqIcAWsgd4myCCiZbWBsuJ8P0gtarFIpleNacCc6OV3i2Rg==',
-        SIGN: 'AKAQEUiQptgpd0lKcXbuggGXH/dCdQCgdysrTBLUlbeGAQ4vwrnLOqSL7+c=' 
-    } 
+        SIGN: 'AKAQEUiQptgpd0lKcXbuggGXH/dCdQCgdysrTBLUlbeGAQ4vwrnLOqSL7+c='
+    }
 });
 
 assert.deepEqual(syslogParser.parseSignedCertificate(fullySignedSD['ssign-cert']), {
@@ -34,7 +36,7 @@ assert.deepEqual(syslogParser.parseSignedCertificate(fullySignedSD['ssign-cert']
     payloadTimestamp: new Date('2009-05-03T14:00:39.519005+02:00'),
     payloadType: 'K',
     payloadName: 'Public Key',
-    keyBlob: new Buffer(fullySignedSD['ssign-cert']['FRAG'].split(/\s/)[2], encoding='base64'),
-    thisSignature:  new Buffer(fullySignedSD['ssign-cert']['SIGN'], encoding='base64')
+    keyBlob: bufferFrom(fullySignedSD['ssign-cert']['FRAG'].split(/\s/)[2], encoding='base64'),
+    thisSignature:  bufferFrom(fullySignedSD['ssign-cert']['SIGN'], encoding='base64')
 });
 
